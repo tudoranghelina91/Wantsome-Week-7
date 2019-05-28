372a018610c1b6fee8fc44ba5c86da116ecfd451
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-05-28 20:55:25 +0300

    Created Week 7 project

---
+++ SquareRoot/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IODS.Handlers;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InvalidOperationException invalidOperationException = new InvalidOperationException(message: "Invalid number");
+
+            try
+            {
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                if (x < 0)
+                    throw invalidOperationException;
+
+                Console.WriteLine("{0:F3}", Math.Sqrt(x));
+            }
+
+            catch
+            {
+                OutputHandling.Error("Invalid number");
+            }
+
+            finally
+            {
+                OutputHandling.Message("Good bye!");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
