bd67eefbaee887f7f9ca4fe455d87224e6bfe632
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-05-28 19:13:47 +0300

    Created week 7 project

---
+++ WantsomeWeek7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WantsomeWeek7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string s = Console.ReadLine();
+                var x = int.Parse(s);
+                Console.WriteLine("Valid integer number {0}", x);
+
+                if (x == 0)
+                    throw new Exception("Number is 0");
+            }
+
+            catch (FormatException e)
+            {
+                Console.WriteLine("XXX - FORMAT EXCEPTION");
+            }
+
+            catch (OverflowException e)
+            {
+                Console.WriteLine("OVERFLOOOOOOOOOOOOOOOOOWWW!!!");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
