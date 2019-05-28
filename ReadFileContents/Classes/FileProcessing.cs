165d5501dbfdef4474e5aaed8a24d1712ca965fc
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-05-28 23:28:03 +0300

    Resolved Read FIle Contents Exercise

---
+++ ReadFileContents/Classes/FileProcessing.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IODS.Handlers;
+using System.IO;
+
+namespace ReadFileContents.Classes
+{
+    static class FileProcessing
+    {
+        public static void ReadFileContents(string filePath)
+        {
+            StringBuilder fileContents = new StringBuilder();
+            fileContents.Append("*** BEGINNING OF FILE***\n\n");
+
+            try
+            {
+                fileContents.Append(File.ReadAllText(filePath));
+                fileContents.Append("\n\n*** END OF FILE***");
+                PrintFileContents(fileContents.ToString());
+            }
+
+            catch(FileNotFoundException)
+            {
+                fileContents.Clear();
+                OutputHandling.Error("FILE NOT FOUND!");
+            }
+
+            catch(DirectoryNotFoundException)
+            {
+                fileContents.Clear();
+                OutputHandling.Error("NO SUCH DIRECTORY!");
+            }
+
+            catch(ArgumentException)
+            {
+                fileContents.Clear();
+                OutputHandling.Error("PATH NAMES CANNOT CONTAIN SPECIAL CHARACTERS OR EMPTY SPACES");
+            }
+
+            catch(UnauthorizedAccessException)
+            {
+                fileContents.Clear();
+                OutputHandling.Error("YOU ARE NOT ALLOWED TO ACCESS THIS PATH");
+            }
+
+            finally
+            {
+                ExitProgram();
+            }
+        }
+
+        public static void PrintFileContents(string fileContents)
+        {
+            OutputHandling.Message(fileContents.ToString());
+        }
+
+        public static void ExitProgram()
+        {
+            OutputHandling.Question("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReadFileContents/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IODS.Handlers;
+using System.IO;
+using ReadFileContents.Classes;
+
+namespace ReadFileContents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = InputHandling.ReadString("Path to file to read: ");
+            FileProcessing.ReadFileContents(filePath);
+        }
+    }
+}
