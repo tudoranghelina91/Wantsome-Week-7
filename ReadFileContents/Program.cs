0f5f036a4513441f9c4f7fe52f188296f488fb25
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-05-31 19:26:08 +0300

    Removed unused namespaces

--- ReadFileContents/Program.cs
+++ ReadFileContents/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using IODS.Handlers;
-using System.IO;
+﻿using IODS.Handlers;
 using ReadFileContents.Classes;
 
 namespace ReadFileContents
