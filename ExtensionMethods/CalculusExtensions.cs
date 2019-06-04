63dfea8eefce61943820d2b79a787ab96dda88b7
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-06-04 18:55:19 +0300

    Added ExtensionMethod course from class

---
+++ ExtensionMethods/CalculusExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class CalculusExtensions
+    {
+        public static void Add(ref this decimal @this, decimal number)
+        {
+            @this += number;
+        }
+
+        public static void Substract(ref this decimal @this, decimal number)
+        {
+            @this -= number;
+        }
+
+        public static void Multiply(ref this decimal @this, decimal number)
+        {
+            @this *= number;
+        }
+
+        public static void Divide(ref this decimal @this, decimal number)
+        {
+            @this /= number;
+        }
+    }
+}
