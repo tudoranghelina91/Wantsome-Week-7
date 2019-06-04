32b6a0f33d6bd3c762dd8ee8b8df7ed42f0255ee
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-06-04 20:34:32 +0300

    Added DelegateMethods

---
+++ DelegateMethodHOWTO/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateMethodHOWTO
+{
+    class Program
+    {
+        public delegate void PrintDelegate(int x);
+        public static void PrintEven(int x)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(x);
+            Console.ResetColor();
+        }
+
+        public static void PrintOdd(int x)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine(x);
+            Console.ResetColor();
+        }
+
+        static void Main(string[] args)
+        {
+            PrintDelegate print;
+
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                    print = PrintEven;
+
+                else
+                    print = PrintOdd;
+
+                print(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DelegateMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateMethods
+{
+    class Program
+    {
+        public delegate void PrintEven()
+        {
+
+        }
+
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
