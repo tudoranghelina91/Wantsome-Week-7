1577b610bd27590ba5a94b42e2434b2406137e61
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-06-05 13:46:14 +0300

    Created Predicate Delegates examples from https://www.tutorialsteacher.com/csharp/csharp-predicate

---
+++ HomeworkPredicateDelegate/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkPredicateDelegate
+{
+    class Program
+    {
+        static bool IsUpperCase(string str)
+        {
+            return str.Equals(str.ToUpper());
+        }
+        static void Main(string[] args)
+        {
+            // EXAMPLE: PREDICATE DELEGATE
+            Predicate<string> isUpper = IsUpperCase;
+
+            bool result = isUpper("hello world!");
+            Console.WriteLine(result);
+
+            // EXAMPLE: PREDICATE DELEGATE WITH ANONYMOUS METHOD
+            Predicate<string> isUpperAnon = delegate (string s)
+                                            {
+                                                return s.Equals(s.ToUpper());
+                                            };
+
+            bool resultanon = isUpperAnon("hello world!");
+            Console.WriteLine(resultanon);
+
+            // EXAMPLE PREDICATE DELEGATE WITH LAMBDA EXPRESSION
+            Predicate<string> isUpperLambda = s => s.Equals(s.ToUpper());
+            bool resultlambda = isUpperLambda("HELLO WORLD!");
+            Console.WriteLine(resultlambda);
+            Console.ReadKey();
+        }
+    }
+}
