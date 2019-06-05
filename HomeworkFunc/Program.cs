ba0030369d33c3fe8c8df30b23acf8015984fafc
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-06-05 13:19:24 +0300

    Finished examples for https://www.tutorialsteacher.com/csharp/csharp-func-delegate

--- HomeworkFunc/Program.cs
+++ HomeworkFunc/Program.cs
@@ -18,6 +18,23 @@ namespace HomeworkFunc
             Func<int, int, int> add = Sum;
             int result = add(10, 10);
             Console.WriteLine(result);
+
+            // FUNC WITH ANONYMOUS METHOD
+            Func<int> getRandomNumber = delegate ()
+                                        {
+                                            Random rnd = new Random();
+                                            return rnd.Next(1, 100);
+                                        };
+
+            // FUNC WITH LAMBDA EXPRESSION
+            Func<int> getRandomNumber2 = () => new Random().Next(1, 100);
+
+            // or
+
+            Func<int, int, int> Sum2 = (x, y) => x + y;
+
+            Console.WriteLine(Sum2(2, 7));
+
             Console.ReadKey();
         }
     }
