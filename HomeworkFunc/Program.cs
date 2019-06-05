7cf1c901c69eeaf4bac7e9fdbdafbf387bb09090
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-06-05 13:10:13 +0300

    Added HomeworkFunk first example from https://www.tutorialsteacher.com/csharp/csharp-func-delegate

--- HomeworkFunc/Program.cs
+++ HomeworkFunc/Program.cs
@@ -8,8 +8,17 @@ namespace HomeworkFunc
 {
     class Program
     {
+        static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
         static void Main(string[] args)
         {
+            Func<int, int, int> add = Sum;
+            int result = add(10, 10);
+            Console.WriteLine(result);
+            Console.ReadKey();
         }
     }
 }
