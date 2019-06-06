d0293f05b9399b07c322eff76d6e2ff5415b9975
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-06-06 17:40:04 +0300

    Created LINQ queries for students

---
+++ HomeworkStudentsLINQ/Classes/Queries.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkStudentsLINQ.Classes
+{
+    class Queries
+    {
+        public static IEnumerable<Student> FirstBeforeLast(List<Student> students)
+        {
+            IEnumerable<Student> firstBeforeLast = from student in students
+                              where (student.FirstName.CompareTo(student.LastName) > 0)
+                              select student;
+
+            return firstBeforeLast;              
+        }
+
+        public static IEnumerable<Student> AgeRange(List<Student> students)
+        {
+            IEnumerable<Student> ageRange = from student in students
+                              where (student.Age >= 18 && student.Age <= 24)
+                              select student;
+
+            return ageRange;                 
+        }
+
+        public static IEnumerable<Student> OrderStudents(List<Student> students)
+        {
+            IEnumerable<Student> orderStudents;
+
+            orderStudents = students.OrderByDescending(student => student.FirstName)
+                                    .ThenByDescending(student => student.LastName);
+
+            return orderStudents;
+        }
+
+        public static IEnumerable<Student> OrderStudentsLINQ(List<Student> students)
+        {
+            IEnumerable<Student> orderStudents = from student in students
+                            orderby student.FirstName descending
+                            orderby student.LastName descending
+                            select student;
+
+            return orderStudents;
+        }
+
+        public static IEnumerable<Student> StudentGroup(List<Student> students)
+        {
+            IEnumerable<Student> studentGroups = students.Where(student => student.GroupNumber == 2)
+                                    .OrderBy(student => student.FirstName);
+
+            return studentGroups;
+        }
+
+        public static IEnumerable<Student> StudentGroupLINQ(List<Student> students)
+        {
+            IEnumerable<Student> studentGroups = from student in students
+                            where student.GroupNumber == 2
+                            orderby student.FirstName
+                            select student;
+
+            return studentGroups;
+        }
+
+        public static IEnumerable<Student> StudentsByEmail(List<Student> students)
+        {
+            IEnumerable<Student> studentsByEmail = from student in students
+                              where student.Email.EndsWith("abv.bg")
+                              select student;
+
+            return studentsByEmail;
+        }
+
+        public static IEnumerable<Student> StudentsByPhone(List<Student> students)
+        {
+            IEnumerable<Student> studentsByPhone = from student in students
+                              where student.Tel != "" && student.City == "Sofia"
+                              select student;
+
+            return studentsByPhone;
+        }
+
+        public static IEnumerable<Student> StudentsByMarks(List<Student> students)
+        {
+            var studentMarks = from student in students
+                               where student.Marks.Contains(6)
+                               select new
+                               {
+                                   FullName = student.FirstName + " " + student.LastName,
+                                   student.Marks
+                               };
+
+            return studentMarks as IEnumerable<Student>;
+        }
+
+        public static IEnumerable<Student> StudentsWithTwoMarks(List<Student> students)
+        {
+            IEnumerable<Student> studentsWithTwoMarks = students.Where(student => student.Marks.Count == 2);
+            return studentsWithTwoMarks;
+        }
+
+        public static List<float> ExtractMarks(List<Student> students)
+        {
+            IEnumerable<Student> studentsEnrolledInYear = students.Where(student => student.FN / 1000 % 10 == 6 && student.FN / 10000 % 10 == 0);
+            List<float> marks = new List<float>();
+            foreach (Student student in studentsEnrolledInYear)
+                foreach (float mark in student.Marks)
+                    marks.Add(mark);
+
+            return marks;
+        }
+
+        public static IEnumerable<Student> Groups(List<Student> students, List<Group> groups)
+        {
+            IEnumerable<Student> studentsInMath = from student in students
+                                                  join g in groups
+                                                  on student.GroupNumber equals g.GroupNumber
+                                                  where g.DepartmentName == "Mathematics"
+                                                  select student;
+
+            return studentsInMath;
+        }
+
+        public static IEnumerable<Student> GroupedByGroupNumberLINQ(List<Student> students)
+        {
+            var studentsGroupedByGroupNumber = from student in students
+                                               group student by student.GroupNumber into g
+                                               select g;
+
+
+            return studentsGroupedByGroupNumber as IEnumerable<Student>;
+        }
+
+        public static IEnumerable<Student> GroupedByGroupNumber(List<Student> students)
+        {
+            var studentsGroupedByGroupNumber = students.GroupBy(student => student.GroupNumber);
+
+            return studentsGroupedByGroupNumber as IEnumerable<Student>;
+        }
+    }
+}
---
+++ HomeworkStudentsLINQ/Classes/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkStudentsLINQ.Classes
+{
+    class Student
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public enum Genders { Male, Female };
+        public Genders Gender { get; }
+        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
+        public int FN { get; set; }
+        public string Tel { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public List<float> Marks { get; set; } = new List<float>();
+        public int GroupNumber { get; set; }
+
+        private int EnrollmentYear;
+
+        private static Random rnd = new Random();
+
+        public override int GetHashCode()
+        {
+            int fn = 0;
+
+            for (int i = 1; i <= 9; i++)
+            {
+                if (i == 5)
+                    fn = fn * 10 + (EnrollmentYear / 10 % 10);
+
+                else if (i == 6)
+                    fn = fn * 10 + (EnrollmentYear % 10);
+
+                else
+                    fn = fn * 10 + rnd.Next(0, 9);
+            }
+
+            return fn;
+        }
+
+        public Student(string firstName, string lastName, Genders gender, int age, int groupNumber, int enrollmentYear, string tel = "")
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Gender = gender;
+            Age = age;
+            Tel = tel;
+            GroupNumber = groupNumber;
+            EnrollmentYear = enrollmentYear;
+            FN = GetHashCode();
+        }
+    }
+}
