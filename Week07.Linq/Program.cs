de39637649963c135d9d3d3351ff68b4867b79f0
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-06-07 14:39:30 +0300

    Implemented more queries into Week07.Linq

--- Week07.Linq/Program.cs
+++ Week07.Linq/Program.cs
@@ -23,9 +23,34 @@
             var emails = allUsers.Select(x => x.Email).ToList();
 
             // 2 - find all posts for users having email ending with ".net".
+            var postsUserEndingInNet = from p in allPosts
+                                       join u in allUsers
+                                       on p.UserId equals u.Id
+                                       where u.Email.EndsWith(".net")
+                                       select new
+                                       {
+                                           u.Name,
+                                           u.Email,
+                                           p.Id,
+                                           p.Title,
+                                           p.Body
+                                       };
 
+            Console.WriteLine("Posts of users with email ending in .net");
+            foreach (var p in postsUserEndingInNet)
+            {
+                Console.WriteLine("===========================================");
+                Console.WriteLine($"Username: {p.Name}");
+                Console.WriteLine($"Email: {p.Email}");
+                Console.WriteLine($"PostID: {p.Id}");
+                Console.WriteLine($"Post Title: {p.Title}");
+                Console.WriteLine("===========================================");
+                Console.WriteLine($"{p.Body}");
+                Console.WriteLine();
+            }
 
             // 3 - print number of posts for each user.
+
 
             // 4 - find all users that have lat and long negative.
             Console.WriteLine("Users with negative long and lat");
@@ -52,15 +77,59 @@
             Console.WriteLine(thelongestpost.Body);
 
             // 6 - print the name of the employee that have post with longest body.
+            var employeeWithLongestBody = from p in allPosts
+                                           join u in allUsers
+                                           on p.UserId equals u.Id
+                                           orderby p.Body.Length descending
+                                           select new
+                                           {
+                                               u.Name,
+                                               u.Email,
+                                               p.Id,
+                                               p.Title,
+                                               p.Body
+                                           };
 
+            Console.WriteLine("EMPLOYEE WITH LONGEST POST:");
+            Console.WriteLine(employeeWithLongestBody.First().Name);
 
             // 7 - select all addresses in a new List<Address>. print the list.
+            IEnumerable<Address> addresses = new List<Address>();
+            addresses = from address in allUsers
+                        select address.Address;
+
+            Console.WriteLine("LIST OF ADDRESSES:");
+            foreach (Address address in addresses)
+            {
+                Console.WriteLine("===============================");
+                Console.WriteLine($"City: {address.City}");
+                Console.WriteLine($"Street: {address.Street}");
+                Console.WriteLine($"Suite: {address.Suite}");
+                Console.WriteLine($"ZIP: {address.Zipcode}");
+                Console.WriteLine();
+            }
 
 
             // 8 - print the employee with min lat
+            var employeeMinLat = from u in allUsers
+                                 orderby u.Address.Geo.Lat
+                                 select u;
+
+
+            Console.WriteLine("Employee Minimum lat");
+            Console.WriteLine(employeeMinLat.First().Name);
+            Console.WriteLine(employeeMinLat.First().Address.Geo.Lat);
 
 
             // 9 - print the employee with max long
+            var employeeMaxLong = from u in allUsers
+                                 orderby u.Address.Geo.Lng descending
+                                 select u;
+
+
+            Console.WriteLine("Employee MAX LNG");
+            Console.WriteLine(employeeMaxLong.First().Name);
+            Console.WriteLine(employeeMaxLong.First().Address.Geo.Lng);
 
 
             // 10 - create a new class: public class UserPosts { public User User {get; set}; public List<Post> Posts {get; set} }
