9414d5f1bfd2d9477596706e01b102b982cdd911
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-06-08 20:25:00 +0300

    Resolved Timer Exercise

---
+++ HomeworkTimer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkTimer
+{
+    class Program
+    {
+        public static void MyMethod ()
+        {
+            Console.WriteLine("I have printed something, bby!");
+        }
+
+        static void Main(string[] args)
+        {
+            Timer myTimer = new Timer(1, MyMethod);
+        }
+    }
+}
---
+++ HomeworkTimer/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace HomeworkTimer
+{
+    class Timer
+    {
+        //public delegate void Print<T>(T value);
+        static Stopwatch stopwatch = new Stopwatch();
+
+        public Timer(int seconds, Action print)
+        {
+            stopwatch.Start();
+            Console.WriteLine("Method will now begin executing. Press any key to quit at any time...");
+            while (!Console.KeyAvailable)
+            {
+                if (stopwatch.ElapsedMilliseconds == seconds * 1000)
+                {
+                    Console.Write($"Method executed at {DateTime.Now} - Output: ");
+                    print();
+                    stopwatch.Restart();
+                }
+                // do something
+            }
+        }
+    }
+}
